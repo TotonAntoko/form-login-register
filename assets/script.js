91d3ba7a6f5298b44897d0cb5afdb2711dcd1a24
Toton Dwi Antoko <Totonantoko@gmail.com>
2021-08-16 10:06:18 +0700

    add interactive label for input

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+const inputs = document.querySelectorAll("input");
+
+
+function addFocusClass() {
+  let parent = this.parentNode.parentNode;
+  parent.classList.add("focus");
+}
+
+function removeFocusClass() {
+  let parent = this.parentNode.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach(input => {
+  input.addEventListener("focus", addFocusClass);
+  input.addEventListener("blur", removeFocusClass);
+});
